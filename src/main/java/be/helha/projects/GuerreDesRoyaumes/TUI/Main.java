d3e2104f958065ab04b7569a0dcafc7c5f2121c4
la226247 <la226247@student.helha.be>
2025-04-30 21:44:27 +0200

    Ajout Main.java pour test de base

---
+++ src/main/java/be/helha/projects/GuerreDesRoyaumes/TUI/Main.java
@@ -0,0 +1,8 @@
+package be.helha.projects.GuerreDesRoyaumes.TUI;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans Guerre des Royaumes !");
+        // Ici, on pourra lancer le menu TUI ou autre plus tard
+    }
+}
