e2caa977035a38cf5754ed0c5052e5b0914c66e4
la226247 <la226247@student.helha.be>
2025-05-17 21:19:54 +0200

    Ajout de classes GsonObjectIdAdapter et ObjectIdAdapter pour la sérialisation et désérialisation des ObjectId avec Gson. Ces adaptateurs personnalisés permettent de gérer les conversions entre ObjectId et JSON.

---
+++ src/main/java/be/helha/projects/GuerreDesRoyaumes/Outils/GsonObjectIdAdapter.java
@@ -0,0 +1,15 @@
+package be.helha.projects.GuerreDesRoyaumes.Outils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.bson.types.ObjectId;
+
+public class GsonObjectIdAdapter {
+
+    public static Gson getGson() {
+        // Création d'une instance Gson avec un adaptateur personnalisé pour ObjectId
+        return new GsonBuilder()
+                .registerTypeAdapter(ObjectId.class, new ObjectIdAdapter())
+                .create();
+    }
+}
